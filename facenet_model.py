155db565c4d445ded5bb0f51ef1e35abeba8a9eb
Edouard Castets <castetscrassoux@yahoo.fr>
2022-12-10 15:07:29 +0100

    Cleaning code

--- facenet_model.py
+++ facenet_model.py
@@ -3,25 +3,44 @@ import torch.nn as nn
 
 
 class FaceNet(nn.Module):
-    
-    def __init__(self, dropout:float, dropout_inter:float,init_weights:bool):
-        super(FaceNet, self).__init__()
-        # self.conv1=nn.Conv2d(3,64,kernel_size= 7,stride=2, padding=3)
-        # self.maxpool1=nn.MaxPool2d(3, stride=2)
-        self.conv1=ConvModule(3,3,1,1,0)#56
-        self.conv2=ConvModule(3,64,1,1,0)#56
-        self.conv3=ConvModule(64,192,3,1,1)
-        self.maxpool1=nn.MaxPool2d(3,2,1)#28
-        self.inception1a=InceptionModule(192,64,96,128,16,32,32)#28
-        self.inception1b=InceptionModule(256,128,128,192,32,96,64)#28
-        self.maxpool2=nn.MaxPool2d(3,2,1)#14
+    """
+    Model based on GoogLeNet (Inception v1) architecture and on facenet model architecture
+    The first layers of GoogLeNet (Conv & Maxpool) have been removed as input image of our project
+    is a 3x56x56 image.
+        -> FaceNet paper : https://arxiv.org/abs/1503.03832
+        -> GoogLeNet paper : https://arxiv.org/abs/1409.4842v1
 
-        self.inception2a=InceptionModule(480,192,96,208,16,48,64)#14
+    L2 pooling has been used in inception module as in facenet model.
+    The final layer is a sigmoid (same for intermediate output modules).
+    Input should be a (Nx3x56x56) tensor
+    Output will be a (Nx1) tensor
+
+    Args:
+        droupout (float) : Dropout probability for final module prediction, 
+                should be 0<dropout<1
+        dropout_inter (float) : Dropout probability for intermediate modules 
+                prediction, should be 0<dropout_inter<1
+        init_weights (bool) : If true the weights are initialized with normal
+                and constant distribution
+    """
+    def __init__(self, dropout:float, dropout_inter:float,init_weights:bool):
+
+
+        super(FaceNet, self).__init__()
+        self.conv1=ConvModule(3,3,1,1,0)
+        self.conv2=ConvModule(3,64,1,1,0)
+        self.conv3=ConvModule(64,192,3,1,1)
+        self.maxpool1=nn.MaxPool2d(3,2,1)
+        self.inception1a=InceptionModule(192,64,96,128,16,32,32)
+        self.inception1b=InceptionModule(256,128,128,192,32,96,64)
+        self.maxpool2=nn.MaxPool2d(3,2,1)
+
+        self.inception2a=InceptionModule(480,192,96,208,16,48,64)
         self.inception2b=InceptionModule(512,160,112,224,24,64,64)
         self.inception2c=InceptionModule(512,128,128,256,24,64,64)
         self.inception2d=InceptionModule(512,112,144,288,32,64,64)
-        self.inception2e=InceptionModule(528,256,160,320,32,128,128)#14
-        self.maxpool3=nn.MaxPool2d(3,2,1)#7
+        self.inception2e=InceptionModule(528,256,160,320,32,128,128)
+        self.maxpool3=nn.MaxPool2d(3,2,1)
 
         self.inception3a=InceptionModule(832,256,160,320,32,128,128)
         self.inception3b=InceptionModule(832,384,192,384,48,128,128)
@@ -99,7 +118,18 @@ class FaceNet(nn.Module):
 
 
 class ConvModule(nn.Module):
+    """
+    Convolutionnal module : Conv2d + BatchNorm2d + ReLu
+    Input should be a (NxC1xH1xH1) tensor
+    Output will be a (NxC2xH2xH2) tensor
 
+    Args:
+        in_c (int): number of channel of the input = C1
+        out_c (int): number of channel of the output = C2
+        kernel_size (int): kernel size for the convolution
+        stride(int): stride for the convolution
+        padding(int): padding for the convolution
+    """
     def __init__(self, in_c: int, out_c: int, kernel_size: int, stride: int, padding= int):
         super(ConvModule, self).__init__()
         self.conv = nn.Conv2d(in_c, out_c, kernel_size, stride, padding,  bias=False)
@@ -115,7 +145,17 @@ class ConvModule(nn.Module):
 
 
 class L2Pooling(nn.Module):
+    """
+    L2 Pooling Layer
+    Input should be a (NxCxH1xH1) tensor
+    Output will be a (NxCxH2xH2) tensor
 
+    Args:
+        kernel_size (int): kernel size used for the pooling
+        stride (int): stride used for the pooling
+        padding (int): padding used for the pooling
+    
+    """
     def __init__(self, kernel_size: int, stride: int, padding= int):
         super(L2Pooling, self).__init__()
         self.pool = nn.AvgPool2d(kernel_size=kernel_size, stride=stride,padding=padding)
@@ -126,6 +166,21 @@ class L2Pooling(nn.Module):
 
 
 class InceptionModule(nn.Module):
+    """
+    Inception Module as described in GoogLeNet paper
+    Input should be a (NxC1xHxH) tensor
+    Output will be a (NxC2xHxH) tensor, it is the result of concatenation
+    of 4 branch, C2=ch1+ch3=ch5+chpool
+
+    Args:
+        in_c(int): number of channel of the input = C1
+        ch1(int): number of channel of the output of the 1x1 branch
+        ch3_red(int): number of channel of reduction conv in the 3x3 branch
+        ch3(int): number of channel of the output of the 3x3 branch
+        ch5_red(int): number of channel of reduction conv in the 5x5 branch
+        ch5(int): number of channel of the output of the 5x5 branch
+        chpool(int): number of channel of the output of the pooling branch
+    """
 
     def __init__(self, in_c: int, ch1: int, ch3_red: int, ch3: int, ch5_red: int, ch5: int, chpool: int):
 
@@ -152,6 +207,17 @@ class InceptionModule(nn.Module):
         return torch.cat([branch1,branch2,branch3,branch4],1)
 
 class InceptionInterModule(nn.Module):
+    """
+    Inception Intermediate Module, used only during training
+    Input is a (NxCxHxH) tensor
+    Output is a (Nx1) tensor
+    Final layer is a sigmoid
+    
+    Args:
+        in_c(int): number of channel of the input = C
+        dropout(float):  Dropout probability for intermediate module
+                prediction, should be 0<dropout<1
+    """
     def __init__(self, in_c: int, dropout: float):
         super(InceptionInterModule, self).__init__()
         self.out=nn.Sequential(
---
+++ utils.py
@@ -0,0 +1,117 @@
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset
+import torch
+import tqdm
+import numpy as np
+
+class dataset(Dataset):
+    def __init__(self, data_0, data_1):
+        self.samples = []
+        self.transform=transforms.Compose([  transforms.RandomHorizontalFlip(p=0.5),
+                                transforms.RandomAffine(20,(0.12,0.12),(0.8,1.2),interpolation=transforms.InterpolationMode.NEAREST,fill=0),
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                                ])
+        for img in data_0:
+            img=transforms.ToTensor()(img)
+            self.samples.append((img,0))
+        for img in data_1:
+            img=transforms.ToTensor()(img)
+            self.samples.append((img,1))
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, id):
+        (img,label)=self.samples[id]
+        img=self.transform(img)
+        return (img,label)
+
+
+def lossfunction(x,y):
+    num_0=torch.where(y==0)[0].shape[0]
+    num_1=x.shape[0]-num_0
+    w0=0.
+    w1=0.
+    if num_0!=0:
+        w0=torch.divide(x.shape[0],(2*num_0),).float()
+    if num_1!=0:
+        w1=torch.divide(x.shape[0],(2*num_1),).float()
+    w=torch.ones_like(x).float()
+    w[torch.where(y==0)[0]]=w0
+    w[torch.where(y==1)[0]]=w1
+    return torch.nn.BCELoss(w)(x.float(),y.float())
+
+def training(num_epochs,facenet,optimizer,scheduler,dataloader_t,dataloader_v,device,alpha1,alpha2,threshold,path):
+
+    Loss_train_train=[]
+    Loss_train_eval=[]
+    Loss_val_eval=[]
+    for epoch in tqdm.tqdm(range(num_epochs)):
+        L_t_t=[]
+        L_t_e=[]
+        L_v_e=[]
+        tp_t,fn_t,tn_t,fp_t=0,0,0,0
+        tp_v,fn_v,tn_v,fp_v=0,0,0,0
+
+        facenet.train()
+        for i, dataj in (enumerate(dataloader_t, 0)):
+            facenet.zero_grad()
+            x=dataj[0].float().to(device)
+            gt=dataj[1].float().to(device)
+            y,aux1,aux2=facenet(x)
+            loss=lossfunction(y.view(-1),gt)
+            loss_aux1=lossfunction(aux1.view(-1),gt)
+            loss_aux2=lossfunction(aux2.view(-1),gt)
+            total_loss=(loss+alpha1*loss_aux1+alpha2*loss_aux2)/(1+alpha1+alpha2)
+            total_loss.backward()
+            optimizer.step()
+            L_t_t.append([loss.item(),loss_aux1.item(),loss_aux2.item(),total_loss.item()])
+            
+        facenet.eval()
+        for i, dataj in enumerate(dataloader_t, 0):
+            x=dataj[0].float().to(device)
+            yh=dataj[1].float().to(device)
+            y=facenet(x).view(-1)
+            loss=lossfunction(y,gt)
+            L_t_e.append(loss.item())
+            pred=y>threshold
+            gt=yh
+            tp_t+=torch.sum(((y>0.5)==gt)[torch.where((gt)==1)]).item()#TP
+            fn_t+=torch.sum(((y>0.5)!=gt)[torch.where((gt)==1)]).item()#FN
+            tn_t+=torch.sum(((y>0.5)==gt)[torch.where((gt)==0)]).item()#TN
+            fp_t+=torch.sum(((y>0.5)!=gt)[torch.where((gt)==0)]).item()#FP
+        for i, dataj in enumerate(dataloader_v, 0):
+            x=dataj[0].float().to(device)
+            gt=dataj[1].float().to(device)
+            y=facenet(x).view(-1)
+            loss=lossfunction(y,gt)
+            L_v_e.append(loss.item())
+            pred=y>threshold
+            tp_v+=torch.sum(((y>0.5)==gt)[torch.where((gt)==1)]).item()#TP
+            fn_v+=torch.sum(((y>0.5)!=gt)[torch.where((gt)==1)]).item()#FN
+            tn_v+=torch.sum(((y>0.5)==gt)[torch.where((gt)==0)]).item()#TN
+            fp_v+=torch.sum(((y>0.5)!=gt)[torch.where((gt)==0)]).item()#FP
+        far_v=fp_v/(tn_v+fp_v)
+        frr_v=fn_v/(tp_v+fn_v)
+        far_t=fp_t/(tn_t+fp_t)
+        frr_t=fn_t/(tp_t+fn_t)
+        hter_t=0.5*(far_t+frr_t)
+        hter_v=0.5*(far_v+frr_v)
+        scheduler.step()
+        err_t_t=np.mean(L_t_t,0)
+        err_t_e=np.mean(L_t_e,0)
+        err_v_e=np.mean(L_v_e,0)
+        Loss_train_train.append(err_t_t)
+        Loss_train_eval.append(err_t_e)
+        Loss_val_eval.append(err_v_e)
+        print("Training : Loss {} ||| Hter {} \t Validation : Loss {} ||| Hter {}".format(err_t_e,hter_t,err_v_e,hter_v))
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': facenet.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'loss_train_train': Loss_train_train,
+            'loss_train_eval': Loss_train_eval,
+            'loss_val_eval':Loss_val_eval,
+            'metrics_train':{'tp':tp_t,'fn':fn_t,'fp':fp_t,'tn':tn_t,'far':far_t,'frr':frr_t,'hter':hter_t},
+            'metrics_val':{'tp':tp_v,'fn':fn_v,'fp':fp_v,'tn':tn_v,'far':far_v,'frr':frr_v,'hter':hter_v}
+            }, path+"checkpoint_{}.pth".format(epoch))
